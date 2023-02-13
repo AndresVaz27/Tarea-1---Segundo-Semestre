3871baa8f10bbb7efdd2e9e3be231869bb6cff28
Andres <andresvazquezo1@hotmail.com>
2023-02-13 07:36:52 -0600

    Add project files.

---
+++ Tarea 1 - Segundo Semestre (2)/Gato.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_1___Segundo_Semestre__2_
+{
+    public class Gato
+    {
+        public string nombre;
+        public int edad;
+        public string juguetefav;
+        public string color;
+        public string comidafav;
+
+        public void Guardarnom()
+        {
+            Console.WriteLine("Se ha guardado el nombre de tu Mascota: " + nombre);
+        }
+        public void Guardaredad()
+        {
+            Console.WriteLine("Se ha guardado la edad de tu Mascota: " + edad);
+        }
+        public void Jugar() 
+        {
+            Console.WriteLine(nombre + " juega con su juguete favorito ---> " + juguetefav);
+        }
+        public void Pintura()
+        {
+            Console.WriteLine(nombre + " tiro un bote de pintura y ya no es color " + color);
+        }
+        public void Comer()
+        {
+            Console.WriteLine(nombre + " se puso triste y fue a comer su comida favorita para terminar el dia.");
+        }
+    }
+}
---
+++ Tarea 1 - Segundo Semestre (2)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_1___Segundo_Semestre__2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gato gato = new Gato();
+            Console.WriteLine("Bienvenido al memorizador de Mascotas Felinas.\nCual es el nombre de tu gato?");
+            gato.nombre = Console.ReadLine(); gato.Guardarnom(); Console.WriteLine("Presiona ENTER para continuar."); Console.ReadKey(); Console.Clear();
+            Console.WriteLine("Cuantos años tiene?");
+            gato.edad = int.Parse(Console.ReadLine()); gato.Guardaredad(); Console.WriteLine("Presiona ENTER para continuar.");Console.ReadKey(); Console.Clear();
+            Console.WriteLine("Cual es su juguete favorito?");
+            gato.juguetefav = Console.ReadLine();Console.Clear();
+            Console.WriteLine("De que color es?");
+            gato.color = Console.ReadLine();Console.Clear();
+            Console.WriteLine("Cual es su comida favorita?");
+            gato.comidafav= Console.ReadLine();Console.Clear();
+            gato.Jugar();gato.Pintura();gato.Comer();
+            Console.ReadKey();
+        }
+    }
+}
